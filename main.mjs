4bfe9bf4b2241b6abe249466dfea3700e13aade7
Akki <aakash.sharma413@gmail.com>
2025-01-19 01:15:43 +0530

    ES Module

---
+++ main.mjs
@@ -0,0 +1,8 @@
+// import math from "./math-esm.mjs";
+// import * as math from "./math-esm.mjs";
+import { add, subtract } from "./math-esm.mjs";
+
+// const { add, subtract } = math;
+
+console.log(add(2, 3));
+console.log(subtract(2, 3));
