5e77b0731d2fbea3bb51dd5d8fd55ef8451db4d9
Akki <aakash.sharma413@gmail.com>
2025-01-19 03:05:34 +0530

    path: builtin module methods

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const data = require("./data.json");
+const path = require("node:path");
+// const path = require("path");
 
-console.log(data.name);
-console.log("Hello Akash Sharma")
-console.log("Welcome to the world of coding!")
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.basename(__dirname));
+console.log(path.basename(__filename));
+
+console.log(path.extname(__dirname));
+console.log(path.extname(__filename));
+
+console.log(path.join("folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "//folder2", "index.html"));
+console.log(path.join("/folder1", "folder2", "../index.html"));
+console.log(path.join("/folder1", "//folder2", "../index.html"));
+
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "../index.html"));
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+console.log(path.resolve(__dirname, "index.html"));
+
+console.log(path.parse(__filename));
+console.log(path.format(path.parse(__filename)));
+console.log(path);
+
+console.log(path.isAbsolute(__dirname));
+console.log(path.isAbsolute("./index.html"));
