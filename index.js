b97bd4470101ae7d4498331d5c75827e38ede8e5
Akki <aakash.sharma413@gmail.com>
2025-01-24 23:16:03 +0530

    pipes

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const fs = require("node:fs");
+const zlib = require("node:zlib");
+
+const gzip = zlib.createGzip();
 
 const readableStreams = fs.createReadStream("./file.txt", {
   encoding: "utf-8",
   highWaterMark: 2
 });
 
+readableStreams.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"))
+
 const writeableStreams = fs.createWriteStream("./file2.txt");
 
-readableStreams.on("data", (chunk) => {
-  console.log(chunk);
-  writeableStreams.write(chunk);
-});
+// readableStreams.on("data", (chunk) => {
+//   console.log(chunk);
+//   writeableStreams.write(chunk);
+// });
+
+readableStreams.pipe(writeableStreams);
