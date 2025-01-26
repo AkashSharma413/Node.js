c7869ec2a1edb400ffc1feb69f4fda626babd40f
Akki <aakash.sharma413@gmail.com>
2025-01-27 00:07:59 +0530

    HTML Response

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const myDetails = {
-    firstName: "Akash",
-    lastName: "Sharma"
-  };
-
-  res.writeHead(200, {"content-type": "application/json"});
-  res.end(JSON.stringify(myDetails));
+  res.writeHead(200, {"content-type": "text/html"});
+  fs.createReadStream(__dirname + "/index.html").pipe(res);
+  // const result = fs.readFileSync("./index.html", "utf-8");
+  // res.end(result);
 })
 
 server.listen(3000, () => {
