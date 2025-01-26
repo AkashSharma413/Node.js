b203cff9e65265fc0ae6c4ef61d29b3b8bc8c014
Akki <aakash.sharma413@gmail.com>
2025-01-27 00:23:29 +0530

    HTML Template

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
+  let name = "Akash Sharma"
   res.writeHead(200, {"content-type": "text/html"});
-  fs.createReadStream(__dirname + "/index.html").pipe(res);
-  // const result = fs.readFileSync("./index.html", "utf-8");
-  // res.end(result);
+  let html = fs.readFileSync("./index.html", "utf-8");
+  html = html.replace("{{name}}", name);
+  res.end(html);
 })
 
 server.listen(3000, () => {
