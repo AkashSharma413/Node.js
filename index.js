6bc772f97d08f1e2ad8121d17df23187150cc728
Akki <aakash.sharma413@gmail.com>
2025-01-27 00:46:10 +0530

    HTTP Routing

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const http = require("node:http");
-const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  let name = "Akash Sharma"
-  res.writeHead(200, {"content-type": "text/html"});
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  res.end(html);
-})
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("About page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        firstName: "Akash",
+        lastName: "Sharma",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not found!");
+  }
+});
 
 server.listen(3000, () => {
   console.log("Server is listening on port 3000");
-})
+});
