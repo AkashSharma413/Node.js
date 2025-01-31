3f8e74afb2ea87225450beb9835c05bf1f0abc30
Akki <aakash.sharma413@gmail.com>
2025-01-31 10:15:19 +0530

    Network I/O

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-// const fs = require("node:fs");
-
-// console.log("first");
-
-// fs.readFile("./file.txt", "utf-8", (err, data) => {
-//   console.log("File Contents");
-// });
-
-// console.log("second");
-
-const crypto = require("node:crypto");
-
-// const start = Date.now();
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-// crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-
-// console.log(`Hash ${Date.now() - start}`);
-
-process.env.UV_THREADPOOL_SIZE = 16;
+const https = require("node:https");
 
 let MAX_CALL = 16;
 
 const start = Date.now();
 for(let i = 0; i < MAX_CALL; i++){
-  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log(`Hash - ${i + 1} ${Date.now() - start}`);
+  https.request("https://www.google.com/", (res) => {
+    res.on("data", () => {})
+    res.on("end", () => {
+      console.log(`Request - ${i + 1}`, Date.now() - start);
+    })
   })
+  .end();
 }
