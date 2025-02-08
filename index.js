b70fb6d66bf1847991e42f130057832728bf7bda
Akki <aakash.sharma413@gmail.com>
2025-02-09 03:27:08 +0530

    close queue

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const fs = require("node:fs");
+const fs = require("node:fs")
 
-// fs.readFile(__filename, () => {
-//     console.log("this is read file 1");
-//     setImmediate(() => console.log("this is inner setImmediate 1")) // check queues
-//     process.nextTick(() => console.log("this is inner nextTick 1")) // microtask queue/nextTick queue
-//     Promise.resolve().then(() => console.log("this is inner resolve 1")) // microtask queue/promise queue
-// }) // I/O queue
+const readableStream = fs.createReadStream(__filename)
+readableStream.close()
 
-// process.nextTick(() => console.log("this is nextTick 1")); // microtask queue/nextTick queue
-// Promise.resolve().then(() => console.log("this is resolve 1")); // microtask queue/promise queue
-// setTimeout(() => console.log("this is setTimeOut 1"), 0); // timer queue
-// setImmediate(() => console.log("this is setImmediate 1")) // check queues
+readableStream.on("close", () => console.log("this is from readableStream close callback"))
 
-setImmediate(() => console.log("this is setImmediate 1"))
-setImmediate(() => {
-    console.log("this is setImmediate 2")
-    process.nextTick(() => console.log("this is process.nextTick 1"))
-    Promise.resolve().then(() => console.log("this is promise.resolve 1"))
-})
-setImmediate(() => console.log("this is setImmediate 3"))
-
+setImmediate(() => console.log("this is setImmediate"))
+setTimeout(() => console.log("this is setTimeOut"))
+Promise.resolve().then(() => console.log("this is promise.resole"))
+process.nextTick(() => console.log("this is nextTick"))
