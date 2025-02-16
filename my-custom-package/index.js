ea0d87a2f668cc0835e39bd359a22dda63eef4da
Akki <aakash.sharma413@gmail.com>
2025-02-17 01:06:29 +0530

    global packages nodemon

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -1,7 +1,7 @@
 const swapCase = require("swap-case").swapCase;
 
 const greet = (name) => {
-    console.log(swapCase(`Hello ${name}, Welcome to the world of coding!`))
+    console.log(swapCase(`Hello ${name}, Welcome to the world of coding!!!!`))
 }
 
 greet("Akash");
