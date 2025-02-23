9945db19b1722e1334bed7db719659ed707f04ac
Akki <aakash.sharma413@gmail.com>
2025-02-23 23:34:20 +0530

    Interactive CLI tools

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
-const {argv} = yargs(process.argv);
+// const yargs = require("yargs");
+// const {argv} = yargs(process.argv);
+
+const inquirer = require("inquirer");
 
 const printFiveMoves = async (pokemonName) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
@@ -10,4 +12,12 @@ const printFiveMoves = async (pokemonName) => {
     console.log(moves.slice(0, 5));
 }
 
-printFiveMoves(argv.pokemon);
+const prompt = inquirer.createPromptModule();
+prompt([{
+    type: "input",
+    name: "pokemon",
+    message: "Enter the name of pokemon to print the first 5 moves"
+}]).then((answer) => {
+    const pokemon = answer.pokemon;
+    printFiveMoves(pokemon);
+})
